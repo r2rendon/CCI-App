6f1ef502b6c9bf9050e41265c5ef6a4a61201617
ArielJT2000 <arieltrochezescobar@gmail.com>
2025-09-07 13:15:38 -0600

    cambios ariel

--- lib/main.dart
+++ lib/main.dart
@@ -52,34 +52,6 @@ class MyApp extends StatelessWidget {
         title: 'CCI San Pedro Sula',
         theme: ThemeData(
           primarySwatch: Colors.blue,
-          fontFamily: 'SF Pro Display', // Fallback: 'Roboto' o 'System'
-          useMaterial3: true,
-          textTheme: const TextTheme(
-            displayLarge: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            displayMedium: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            displaySmall: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            headlineLarge: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            headlineMedium: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            headlineSmall: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            titleLarge: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            titleMedium: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            titleSmall: TextStyle(
-                fontFamily: 'SF Pro Display', fontWeight: FontWeight.w700),
-            bodyLarge: TextStyle(fontFamily: 'SF Pro Display'),
-            bodyMedium: TextStyle(fontFamily: 'SF Pro Display'),
-            bodySmall: TextStyle(fontFamily: 'SF Pro Display'),
-            labelLarge: TextStyle(fontFamily: 'SF Pro Display'),
-            labelMedium: TextStyle(fontFamily: 'SF Pro Display'),
-            labelSmall: TextStyle(fontFamily: 'SF Pro Display'),
-          ),
         ),
         home: const SplashScreen(),
       );
